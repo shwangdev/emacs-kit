f9a69bf03446b5a75659a6e795b4df57a2004fd1
wxjeacen <wxjeacen@gmail.com>
2012-04-19 00:06:00 +0800

    add json

---
+++ elpa/json-1.2/json-autoloads.el
@@ -0,0 +1,13 @@
+;;; json-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+(provide 'json-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; json-autoloads.el ends here
